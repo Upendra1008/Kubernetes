bc981ed6dae5f4e72a4cb7e5db403a76ca4b4145
Upendra1008 <upendrakumar236@gmail.com>
2026-06-14 17:59:55 +0530

    Update delete-all.sh

--- LAB Practice/hpa/SCRIPTS/delete-all.sh
+++ LAB Practice/hpa/SCRIPTS/delete-all.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+kubectl delete -f hpa/hpa.yml
+kubectl delete -f notes-app/notes-service.yml
+kubectl delete -f notes-app/notes-app.yml
+kubectl delete -f load-generator/load-generator.yml
+
+kubectl delete namespace notes-ns
+kubectl delete namespace load-ns
