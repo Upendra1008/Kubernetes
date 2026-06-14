b666e03fce6010364e9eefc7e786eea9eccd34c3
Upendra1008 <upendrakumar236@gmail.com>
2026-06-14 17:59:06 +0530

    Update apply-all.sh

--- LAB Practice/hpa/SCRIPTS/apply-all.sh
+++ LAB Practice/hpa/SCRIPTS/apply-all.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+kubectl apply -f namespaces/notes-namespace.yml
+kubectl apply -f namespaces/load-namespace.yml
+
+kubectl apply -f notes-app/notes-app.yml
+kubectl apply -f notes-app/notes-service.yml
+
+kubectl apply -f hpa/hpa.yml
+
+kubectl apply -f load-generator/load-generator.yml
